db63af1cab83e643932c016a3e461f3a1d23c0e3
jzpeeler <jzpeeler@syr.edu>
2025-03-09 23:39:40 -0400

    Committing part 1 of assignment.

--- code/one_package.py
+++ code/one_package.py
@@ -6,3 +6,27 @@ Calculate the total package size and display that.
 
 see one_package.png for a screenshot
 '''
+import streamlit as st
+import packaging
+
+st.title("Process One Package")
+
+pkg = st.text_input("Enter package data: ")
+
+if st.button("Process Package"):
+    if pkg:
+        parsed_pkg = packaging.parse_packaging(pkg)
+        total = packaging.calc_total_units(parsed_pkg) # these three lines process the package data
+        unit = packaging.get_unit(parsed_pkg)
+
+        st.subheader("Parsed Package Info") #these two lines display the parsed package info
+        st.json(parsed_pkg) 
+
+        st.subheader("Package Contents")
+        table_data = [{"Item": list(item.keys())[0], "Quantity": list(item.values())[0]} for item in parsed_pkg]
+        st.table(table_data)
+
+        st.metric(label="Total Package Size", value=f"{total} {unit}")
+
+else:
+    st.warning("You must input package info in order to process")
